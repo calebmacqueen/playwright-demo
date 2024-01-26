311a3cb229328ae0b8cb7803cd3fe616476d3697
Caleb MacQueen <calebmacqueen@gmail.com>
2024-01-25 19:10:52 -0700

    adding tests

--- UITests.cs
+++ UITests.cs
@@ -9,18 +9,63 @@ namespace PlaywrightTests;
 [TestClass]
 public class UITests : PageTest
 {
+    string BASE_URL = "http://was.tamgc.net/";
+
     [TestMethod]
-    public async Task ShouldHaveTheCorrectSlogan()
+    public async Task PageTitleIsDisplayed()
     {
-        await Page.GotoAsync("https://playwright.dev");
-        await Expect(Page.Locator("text=enables reliable end-to-end testing for modern web apps")).ToBeVisibleAsync();
+        await Expect(Page.Locator("#title")).ToBeVisibleAsync();
     }
 
     [TestMethod]
-    public async Task ShouldHaveTheCorrectTitle()
+    public async Task NavigateToUSSMissouri()
     {
-        await Page.GotoAsync("https://playwright.dev");
-        var title = Page.Locator(".navbar__inner .navbar__title");
-        await Expect(title).ToHaveTextAsync("Playwright");
+        await Page.GetByAltText("United States").ClickAsync();
+        await Page.GetByText("USS Missouri").ClickAsync();
+        AssertOnUSSMissouriDetails();
+    }
+
+    [TestMethod]
+    public async Task SearchForUSSMissouri()
+    {
+        var searchBox = Page.Locator("[name='query']").First;
+        await searchBox.FillAsync("USS Missouri");
+        await searchBox.PressAsync("Enter");
+        await Page.GetByText("USS Missouri").ClickAsync();
+        AssertOnUSSMissouriDetails();
+    }
+
+    [TestMethod]
+    public async Task SearchForUSSMissouriWithPartialText()
+    {
+        var searchBox = Page.Locator("[name='query']").First;
+        await searchBox.FillAsync("Missouri");
+        await searchBox.PressAsync("Enter");
+        await Page.GetByText("USS Missouri").ClickAsync();
+        AssertOnUSSMissouriDetails();
+    }
+
+    [TestMethod]
+    public async Task FindUSSMissouriWithAdvancedSearch()
+    {
+        await Page.GetByText("[Advance]").ClickAsync();
+        await Page.Locator("[name='nation']").SelectOptionAsync(new SelectOptionValue { Value = "United States" });
+        await Page.Locator("[name='type']").SelectOptionAsync(new SelectOptionValue { Value = "Battleship" });
+        await Page.Locator("[name='rarity']").SelectOptionAsync(new SelectOptionValue { Value = "Rare" });
+        await Page.Locator("[name='search']").ClickAsync();
+        await Page.GetByText("USS Missouri").ClickAsync();
+        AssertOnUSSMissouriDetails();
+    }
+
+    [TestInitialize]
+    public async Task Initialize()
+    {
+        await Page.GotoAsync(BASE_URL);
+    }
+
+    private async void AssertOnUSSMissouriDetails()
+    {
+        await Expect(Page.GetByText("USS Missouri")).ToBeVisibleAsync();
+        await Expect(Page.GetByText("Task Force - 28/60 - Rare")).ToBeVisibleAsync();
     }
 }
